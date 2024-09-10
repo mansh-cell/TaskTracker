ba4352bd455b471b8014461dd97eda3e1c8509c4
manshi-cell <mishramanshi14@gmail.com>
2024-09-10 13:41:01 +0530

    created the task header

--- tasktracker/src/components/TaskForm.jsx
+++ tasktracker/src/components/TaskForm.jsx
@@ -1,20 +1,26 @@
 import React from "react";
+import "./TaskForm.css";
 const TaskForm = () => {
     return (
         <header className="app_header">
             <form>
-                <input type="text" placeholder="Enter Your Task"></input>
+                <input className="task_input" type="text" placeholder="Enter Your Task"></input>
                 <div className="task_buttons">
-                    <button className="tag">HTML</button>
-                    <button className="tag">CSS</button>
-                    <button className="tag">JS</button>
-                    <button className="tag">React</button>
-                    <select className="Task_Select">
-                        <option value="Todo"> Doing</option>
-                        <option value="Going to do" >Going to do</option>
-                        <option value="done">Done</option>
-                        <button className="task_Submit">+Add Task</button>
-                    </select>
+                    <div>
+                        <button className="tag">HTML</button>
+                        <button className="tag">CSS</button>
+                        <button className="tag">JS</button>
+                        <button className="tag">React</button>
+                    </div>
+                    <div className="task_select">
+                        <select className="task_status">
+                            <option value="Todo"> Doing</option>
+                            <option value="Going to do" >Going to do</option>
+                            <option value="done">Done</option>
+                        </select>
+
+                    </div>
+                    <button className="task_submit">+Add Task</button>
                 </div>
             </form>
         </header>
