ccd7df8f815eac17ad49d62d9f1f4182410ec103
manshi-cell <mishramanshi14@gmail.com>
2024-09-19 17:01:40 +0530

    Tag filter

--- tasktracker/src/components/TagButton.jsx
+++ tasktracker/src/components/TagButton.jsx
@@ -1,8 +1,25 @@
 import React from "react";
 import "./TagButton.css";
-const TagButton=({TagName , selectTag}) =>{
-    return(
-        <button className="tag" type="button" onClick={() =>selectTag(TagName)}>{TagName}</button>              
-    )
-}
+
+const TagButton = ({ tagName, selectTag, selectedtag }) => {
+    const tagStyle = {
+        HTML: { backgroundColor: "#fda821" },
+        CSS: { backgroundColor: "#15d4c8" },
+        JavaScript: { backgroundColor: "#ffd12c" },
+        React: { backgroundColor: "#4cdafc" },
+        default: { backgroundColor: "#f9f9f9" }
+    };
+
+    return (
+        <button
+            className="tag"
+            type="button"
+            style={selectedtag ? tagStyle[tagName] || tagStyle.default : tagStyle.default}
+            onClick={() => selectTag(tagName)}
+        >
+            {tagName}
+        </button>
+    );
+};
+
 export default TagButton;
--- tasktracker/src/components/TaskForm.jsx
+++ tasktracker/src/components/TaskForm.jsx
@@ -1,87 +1,66 @@
 import React, { useState } from "react";
 import "./TaskForm.css";
-import TagButton from "./Tagbutton";
-const TaskForm = () => {
-    // Properly initialize useState with an object, which is not iterable but correct for this case
-    const [taskData, setTaskData] = useState({
-      Task: "",
-      Status: "Todo",
-      Tags : [],
-    });
-  
-    const handleChangeInput = (e) =>{
-    //   const name = e.target.value ;
-    //   const value = e.target.value ;
-    const {name , value} = e.target;
-      setTaskData((prev) =>{
-        return { ...prev , [name]: value};
+import TagButton from "./TagButton";
 
-      });  
+const TaskForm = () => {
+    const [taskData, setTaskData] = useState({
+        Task: "",
+        Status: "Todo",
+        Tags: [],
+    });
+
+    const handleChangeInput = (e) => {
+        const { name, value } = e.target;
+        setTaskData(prev => ({ ...prev, [name]: value }));
     };
 
-    const handleSubmit = (e) =>{
+    const handleSubmit = (e) => {
         e.preventDefault();
-           console.log(taskData);
-    }
+        console.log(taskData);
+    };
 
-    const selectTag = (Tag) => {
-        console.log(Tag);
-        if(taskData.Tags.some(item => item === Tag)){
-            const filteTags = taskData.Tags.filter(item => item!== Tag)
-            setTaskData(prev => {
-                return { ...prev , Tags: filteTags}
-            })
-           
+    const checkTag = (tag) => {
+        return taskData.Tags.some(item => item === tag);
+    };
+
+    const selectTag = (tag) => {
+        console.log("Selecting/Deselecting tag:", tag);
+        if (taskData.Tags.some(item => item === tag)) {
+            const filteredTags = taskData.Tags.filter(item => item !== tag);
+            setTaskData(prev => ({ ...prev, Tags: filteredTags }));
+        } else {
+            setTaskData(prev => ({ ...prev, Tags: [...prev.Tags, tag] }));
         }
-        else{
-            setTaskData((prev)=>{
-                return { ...prev, Tags : [...Tag]};
-            })
-        }
-    }
+    };
 
-   //this is for every input field we were creating the single different function for each
-    // const [task, seTask] = useState("");
-    // const [status , seStatus]= useState("Todo");
-    // const handleChangeTask =(e) =>{
-    //     seTask(e.target.value);
-        
-    // }
-    // const handleChangeStatus = (e) => {
-    //     seStatus(e.target.value)
-    // }
-    // console.log(task,status);
-
-
-      //Here, we are using onsubmit here we can also use onchange?
-            //but using on submit when someone input anything in input box and then enter and also when someone click one 
-            //submit button in both cases this handlesubmit will work. 
-            //if we pass that function in onCLick event, then it will only work on button click.
-
-//in html we have taken same property and function name at line 56 it doesn't give any error it can be different also
     return (
         <header className="app_header">
             <form onSubmit={handleSubmit}>
-                <input className="task_input" type="text" placeholder="Enter Your Task" name = "Task" onChange={handleChangeInput}></input>
+                <input
+                    className="task_input"
+                    type="text"
+                    placeholder="Enter Your Task"
+                    name="Task"
+                    onChange={handleChangeInput}
+                />
                 <div className="task_buttons">
-                    <div>
-                        <TagButton tagName="HTML" selectTag ={selectTag}></TagButton>
-                        <TagButton tagName="CSS" selectTag ={selectTag}></TagButton>
-                        <TagButton tagName="Javascript" selectTag ={selectTag}></TagButton>
-                        <TagButton tagName="React" selectTag ={selectTag}></TagButton>
-                     |</div>
-                    <div className="task_select" >
-                        <select className="task_status" onChange={handleChangeInput} name = "Status">
-                            <option value="Todo"> Doing</option>
-                            <option value="Going to do" >Going to do</option>
-                            <option value="done">Done</option>
-                        </select>
-
-                    </div>
-                    <button className="task_submit">+Add Task</button>
+                    <TagButton tagName="HTML" selectTag={selectTag} selectedtag={checkTag("HTML")} />
+                    <TagButton tagName="CSS" selectTag={selectTag} selectedtag={checkTag("CSS")} />
+                    <TagButton tagName="JavaScript" selectTag={selectTag} selectedtag={checkTag("JavaScript")} />
+                    <TagButton tagName="React" selectTag={selectTag} selectedtag={checkTag("React")} />
+             
+                <div className="task_select">
+                    <select className="task_status" onChange={handleChangeInput} name="Status">
+                        <option value="Todo">Todo</option>
+                        <option value="Going to do">Going to do</option>
+                        <option value="done">Done</option>
+                    </select>
+                    <button className="task_submit" type="submit">+Add Task</button>
                 </div>
+               </div>
             </form>
         </header>
-    )
-}
+    );
+};
+
 export default TaskForm;
